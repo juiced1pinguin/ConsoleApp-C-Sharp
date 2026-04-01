42072698b47bf341de40442e2c856a73fe8c2ef3
pinguineee <sebastiaankock3@gmail.com>
2026-04-01 13:25:32 +0200

    Create Tips voor het weer.cs

---
+++ ConsoleApp C#/ConsoleApp C#/Tips voor het weer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_C_
+{
+    internal class Tips_voor_het_weer
+    {
+        public void start() 
+        { 
+
+        }
+    }
+}
