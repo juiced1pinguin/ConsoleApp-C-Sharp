80a38e6ef41fa022006f8bc47349ae40ad5127ae
pinguineee <sebastiaankock3@gmail.com>
2026-04-13 14:06:59 +0200

    Update Leeftijd Berekenen.cs

--- ConsoleApp C#/ConsoleApp C#/Leeftijd Berekenen.cs
+++ ConsoleApp C#/ConsoleApp C#/Leeftijd Berekenen.cs
@@ -13,7 +13,16 @@ namespace ConsoleApp_C_
             Console.Write("Geboortejaar: ");
             int jaar = int.Parse(Console.ReadLine());
             int leeftijd = DateTime.Now.Year - jaar;
-            Console.WriteLine("Je bent / word " + leeftijd + " jaar oud.");
+            Console.WriteLine("Je bent " + leeftijd + " jaar oud.");
+            if (leeftijd >= 18)
+            {
+                Console.WriteLine("Je bent 18+ en je mag stemmen.");
+            }
+            else
+            {
+                Console.WriteLine("Je bent nog geen 18 en je mag nog niet stemmen.");
+            }
+
         }
     }
 }
