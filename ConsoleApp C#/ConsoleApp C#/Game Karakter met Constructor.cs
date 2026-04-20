a39f323e1fe66291e3689f26e15e93af5bbdde38
pinguineee <sebastiaankock3@gmail.com>
2026-04-20 11:39:36 +0200

    karacter + constructor creation

---
+++ ConsoleApp C#/ConsoleApp C#/Game Karakter met Constructor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_C_
+{
+    internal class Game_Karakter_met_Constructor
+    {
+        public void start()
+        {
+
+        }
+    }
+}
